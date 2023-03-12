1bd98a68c5d1cf44c4ba5b45acff086602932af9
DarkSkeleti <skeleton.of.the.darkness@gmail.com>
2023-03-12 18:44:37 +0100

    Reblace client with newer bot syntax

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 import discord
 from discord import app_commands
+from discord.ext import tasks, commands
 
 from datetime import datetime
 import time
+
+import os
 
 def read_token():
     with open("token.txt", 'r') as f:
@@ -14,24 +17,23 @@ def utc2local(utc):
     offset = datetime.fromtimestamp(epoch) - datetime.utcfromtimestamp(epoch)
     return utc + offset
 
-
+	
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-tree = app_commands.CommandTree(client)
 
 token = read_token()
 
 
 
-@tree.command(name = "say_hello", description = "The bot posts Hello", guild=discord.Object(id=1081651254226325658)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
-async def first_command(interaction):
+@bot.tree.command(name = "say_hello", description = "The bot posts Hello", guild=discord.Object(id=1081651254226325658)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+async def say_hello(interaction):
     await interaction.response.send_message("Hello!")
 	
-@tree.command(name = "message_times", description = "Get a message time statistic", guild=discord.Object(id=1081651254226325658))	
-async def history(interaction, member: discord.Member):
+@bot.tree.command(name = "message_times", description = "Get a message time statistic", guild=discord.Object(id=1081651254226325658))	
+async def message_times(interaction, member: discord.Member):
     counter = 0
     times = [0] * 24
     for channel in interaction.guild.channels:
@@ -44,18 +46,20 @@ async def history(interaction, member: discord.Member):
     await interaction.response.send_message(f'{member.mention} has sent **{counter}** messages in this server. \n Message_Time_Array: **{times}**')
 
 
-@client.event
+@bot.event
 async def on_ready():
-    await tree.sync(guild=discord.Object(id=1081651254226325658))
     print("Ready!")
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run(token)
+
+bot.run(token)
+
+
 
